94b55057813798a57cd46d2d96919a62a3edbb4c
Pratik Singh <pratik00888@gmail.com>
2024-11-05 20:13:13 +0530

    finished backend

--- backend/routes/booksRoutes.js
+++ backend/routes/booksRoutes.js
@@ -14,4 +14,56 @@ router.post("/add", async (req, res) => {
   }
 });
 
+//get request
+router.get("/getBooks", async (req, res) => {
+  let books;
+  try {
+    books = await bookModel.find();
+    res.status(200).json({ books });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//get request by id
+router.get("/getBooks/:id", async (req, res) => {
+  let books;
+  const id = req.params.id;
+  try {
+    books = await bookModel.findById(id);
+    res.status(200).json({ books });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//update book by id
+router.put("/updateBook/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  let book;
+  try {
+    book = await bookModel.findByIdAndUpdate(id, { ...data });
+    await book.save().then(() => {
+      res.status(200).json({ message: "book updated" });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//delete book
+router.delete("/deleteBook/:id", async(req,res)=>{
+    const id = req.params.id;
+    try {
+        await bookModel.findByIdAndDelete(id).then(()=>{
+            res.status(200).json({message:"book deleted"})
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
 module.exports = router;
